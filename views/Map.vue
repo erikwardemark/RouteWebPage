8a970446e75357dd02ce9fdadd4ec2ca28f57454
Erik Wårdemark <erik.wardemark@gmail.com>
2025-07-13 15:07:42 +0200

    Save edited paths

--- views/Map.vue
+++ views/Map.vue
@@ -12,20 +12,27 @@
             :center="center"
             :zoom="14"
         >
-            <Polyline 
+            <Polyline
+            ref="polylineref"
             :options="pathOptions"
-            @update:path="SavePath"
+            @dragend="onPolylineEdited"
+            @insert_at="onPolylineEdited"
+            @remove_at="onPolylineEdited"
+            @set_at="onPolylineEdited"
             />
         </GoogleMap> 
+    <p>new path: {{ updatedPath }}</p>
     
 </template>
 
 <script setup>
 import axios from 'axios';
 import { GoogleMap, Polyline } from 'vue3-google-map'
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router'
 
+const polylineref = ref(null)
+const updatedPath = ref([])
 const center = ref({})
 const pathOptions = ref({
     path: [],
@@ -39,10 +46,11 @@ const router = useRoute()
 const pathId = ref(router.params.id)
 
 async function decodePolyLine() {
-            const url = `http://192.168.1.87:5000/api/route/${pathId.value}/map`//`http://192.168.1.143:5000/api/route/${this.routeid}/map`;
+            const url = `http://192.168.1.87:5000/api/path/${pathId.value}/map`//`http://192.168.1.143:5000/api/route/${this.routeid}/map`;
             const mapData = await axios.get(url)
             pathOptions.value.path = mapData.data.coordinates;
-            center.value = mapData.data.center;       
+            center.value = mapData.data.center;    
+            updatedPath.value = mapData.data.coordinates;   
 }
 
 function EditPath() {
@@ -52,13 +60,46 @@ function EditPath() {
     }
 }
 
-function SavePath(newPath) {
+async function SavePath() {
   pathOptions.value = {
     ...pathOptions.value,
-    path: newPath,
+    path: getNewPath().getArray(),
     editable: false
     }
+    await axios.put(`http://192.168.1.87:5000/api/path/${pathId.value}/map`, 
+    {
+      path: pathOptions.value.path,
+      center: center.value
+    }
+);
+
+
 }
 
-decodePolyLine()
+function onPolylineEdited() {
+  if (polylineref.value) {
+    // Access the underlying MVCArray of the path
+    const newPath = getNewPath();
+    
+    // Get the path as an array of LatLng objects
+    const pathArray = newPath.getArray();
+    
+    updatedPath.value = pathArray.map(latLng => ({
+      lat: latLng.lat(),
+      lng: latLng.lng()
+    }));
+    console.log('Polyline path updated:', updatedPath.value);
+  }
+};
+
+function getNewPath() {
+  // This assumes the Polyline instance has a getPath() method
+  return polylineref.value.polyline.getPath()
+  
+
+}
+onMounted(() => {
+  decodePolyLine()
+})
+
 </script>
